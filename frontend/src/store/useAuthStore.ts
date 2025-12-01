d3255071d434193b254add7584df63ba5489a8c2
Deepak Sharma <deepak.sh798@gmail.com>
2025-12-01 23:58:24 +0530

    add LoginPage

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -32,4 +32,27 @@ export const useAuthStore = create((set: any) => ({
       toast.error(error.response.data.message);
     }
   },
+
+  login: async (data: any) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully");
+    } catch (error: any) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error: any) {
+      toast.error(error.response.data.message);
+    }
+  },
 }));
