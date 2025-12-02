064d159bdc70036a7f35c2c8556be66e6ca4bf17
Deepak Sharma <deepak.sh798@gmail.com>
2025-12-02 23:33:37 +0530

    fix messages/users route

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port 5000");
